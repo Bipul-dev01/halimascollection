0b2278e8fc004978ecc9a61bbcaa4f0133d02618
Tajul <kumar@gmail.com>
2025-06-17 22:58:41 +0600

    added

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         // sticky navbar on scroll script
-//         if(this.scrollY > 20){
-//             $('.header').addClass("sticky");
-//         }else{
-//             $('.header').removeClass("sticky");
-//         }
-//          // scroll-up button show/hide script
-//          if(this.scrollY>500){
-//             $('.scroll-up-btn').addClass("show");
-//          }else{
-//             $('.scroll-up-btn').removeClass("show");
-//          }
-//     });
-// });
+// Preloader
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loading")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 2200)
+}
+
+loaderAnimation()
 
 // Toggle menu
 
